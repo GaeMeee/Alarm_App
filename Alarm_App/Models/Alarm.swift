2acf6dccd3e7dd664a36e6b38bc5ec2fdcf072ea
luttoli <hhn0212@naver.com>
2023-10-04 14:17:25 +0900

    알람 페이지, notification 구현

--- Alarm_App/Models/Alarm.swift
+++ Alarm_App/Models/Alarm.swift
@@ -8,9 +8,20 @@
 import Foundation
 
 struct Alarm: Codable {
-    var setTime: Date
-    var content: String
-    var notificationSound: String
-    var snoozeEnabled: Bool
-    var isNotificationEnabled: Bool
+    var id: String = UUID().uuidString
+    let date: Date
+    var isOn: Bool
+    
+    var time: String {
+        let timeFormatter = DateFormatter()
+        timeFormatter.dateFormat = "hh:mm"
+        return timeFormatter.string(from: date)
+    }
+    
+    var meridiem: String {
+        let meridiemFormatter = DateFormatter()
+        meridiemFormatter.dateFormat = "a"
+        meridiemFormatter.locale = Locale(identifier: "ko")
+        return meridiemFormatter.string(from: date)
+    }
 }
--- Alarm_App/SceneDelegate.swift
+++ Alarm_App/SceneDelegate.swift
@@ -29,9 +29,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
     }
 
+    //
     func sceneDidBecomeActive(_ scene: UIScene) {
-        // Called when the scene has moved from an inactive state to an active state.
-        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+        UIApplication.shared.applicationIconBadgeNumber = 0
     }
 
     func sceneWillResignActive(_ scene: UIScene) {
